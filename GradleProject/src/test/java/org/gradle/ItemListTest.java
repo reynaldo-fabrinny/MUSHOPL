c0f28ef32ebf7f3a77431b19989b7b899ac8482c
Reynaldo Fabrinny <reynaldo.tiburcio@exedio.com>
2015-07-16 12:33:54 +0200

    Removing old Stuff

---
+++ GradleProject/src/test/java/org/gradle/ItemListTest.java
@@ -0,0 +1,15 @@
+package org.gradle;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ItemListTest {
+  
+	
+	@Test
+    public void canConstructAPersonWithAName() {
+      //  Person person = new Person("Larry");
+       // assertEquals("Larry", person.getName());
+    }
+}
