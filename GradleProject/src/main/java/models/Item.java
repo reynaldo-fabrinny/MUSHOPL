689f3b781a0b0dabbfcffd8b3694c8124894a310
Reynaldo Fabrinny <reynaldo.tiburcio@exedio.com>
2015-07-16 13:49:55 +0200

    Improving Item

--- GradleProject/src/main/java/models/Item.java
+++ GradleProject/src/main/java/models/Item.java
@@ -5,18 +5,28 @@ public class Item
 	private final String name;
 	private int quantity;
 	private String brand;
+	private boolean bought;
 	
 	Item(final String name, final int quantity) 
 	{
 		this.name = name;
 		this.quantity = quantity;
+		this.bought = false;
 	}
-	
+
 	Item(String name)
 	{
 		this(name,1);
 	}
 	
+	public boolean isBought() 
+	{
+		return bought;
+	}
+	public void setBought(boolean bought) 
+	{
+		this.bought = bought;
+	}
 	public String getBrand() 
 	{
 		return brand;
@@ -33,5 +43,4 @@ public class Item
 	{
 		return quantity;
 	}
-	
 }
